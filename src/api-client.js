07edc18091b67329bf625509e60a3d1d73997299
midoriq <izuku.midoriq22@gmail.com>
2025-06-02 22:10:19 +0200

    chore: import change

--- src/api-client.js
+++ src/api-client.js
@@ -2,3 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://afdjdcimdzvkaajjbogr.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default superbase;
